9a8d92f8384b8607e847b93f2fc5d743a0f54460
JessCP25 <jpayanoc25@gmail.com>
2024-03-27 20:02:54 -0500

    Inyectando repositorio en caso de uso

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,15 +1,21 @@
 import { CronJob } from "cron";
 import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
-import { error } from "console";
+import { logRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+
+const fileSystemLogRepository = new logRepositoryImpl(
+  new FileSystemDataSource,
+)
 
 export class Server {
   public static start(){
     console.log('Server started...');
 
     CronService.createJob('*/5 * * * * *', ()=>{
-      const url = 'https://google.com'
+      const url = 'https://localhost:3000'
       new CheckService(
+        fileSystemLogRepository,
         ()=> console.log(`${url} is ok`),
         (error)=> console.log(error)
       ).execute(url)
