4788d293f8d19fab8186bf8583a52bc4a526849e
JessCP25 <jpayanoc25@gmail.com>
2024-03-31 21:25:56 -0500

    Creacion y lectura con postgresLogDataSource

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugin";
 import { MongoDataBase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
-(()=>{
+(() => {
   main();
 })();
 
 async function main() {
   await MongoDataBase.connect({
     mongoUrl: envs.MONGO_URL,
-    dbName: envs.MONGO_DB_NAME
+    dbName: envs.MONGO_DB_NAME,
   });
-  
+
+  const prisma = new PrismaClient();
+  // const newLog = await prisma.logModel.create({
+  //   data: {
+  //     level: "MEDIUM",
+  //     message: "Test message 1",
+  //     origin: "App.ts",
+  //   },
+  // });
+
+  // const logs = await prisma.logModel.findMany({
+  //   where: {
+  //     level: 'MEDIUM'cons
+  //   }
+  // });
+  // console.log({ logs });
+
   // Crear un colección = tables, documento = registro
   Server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -8,15 +8,17 @@ import { EmailService } from "./email/email.service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { MongoLogDataSource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogSeverityLevel } from "../domain/entities/log.entity";
+import { PostgresLogDataSource } from "../infrastructure/datasources/postgres-log.datasource";
 
 const logRepository = new logRepositoryImpl(
-  new FileSystemDataSource()
+  // new FileSystemDataSource()
   // new MongoLogDataSource(),
+  new PostgresLogDataSource()
 );
 const emailService = new EmailService();
 
 export class Server {
-  public static  async start() {
+  public static async start() {
     console.log("Server started...");
 
     // Mandar email
@@ -25,22 +27,20 @@ export class Server {
     //   fileSystemLogRepository
     // ).execute('jpayanoc25@gmail.com')
 
-
-
     // emailService.sendEmailWithFileSystemLogs(['jpayanoc25@gmail.com']);
 
-    const logs = await logRepository.getLogs(LogSeverityLevel.medium);
-    console.log(logs);
+    // const logs = await logRepository.getLogs(LogSeverityLevel.medium);
+    // console.log(logs);
 
-    // CronService.createJob('*/5 * * * * *', ()=>{
-    //   const url = 'https://localhost:3000'
-    //   // const url = 'https://google.com'
-    //   new CheckService(
-    //     logRepository,
-    //     ()=> console.log(`${url} is ok`),
-    //     (error)=> console.log(error)
-    //   ).execute(url)
+    CronService.createJob('*/5 * * * * *', ()=>{
+      // const url = 'https://localhost:3000'
+      const url = 'https://google.com'
+      new CheckService(
+        logRepository,
+        ()=> console.log(`${url} is ok`),
+        (error)=> console.log(error)
+      ).execute(url)
 
-    // })
+    })
   }
 }
